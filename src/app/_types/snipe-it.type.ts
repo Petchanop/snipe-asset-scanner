61f1dac0417fa7ec711bc2c22acc1eccf86d99c9
petchanop <nop.hide@gmail.com>
2025-07-01 03:46:54 +0000

    integrate prisma orm

--- src/app/_types/snipe-it.type.ts
+++ src/app/_types/snipe-it.type.ts
@@ -4,3 +4,4 @@ export type HasKey<T, K extends PropertyKey> = K extends keyof T ? true : false
 export type MissingKey<T, K extends PropertyKey> = K extends keyof T ? false : true
 export type Notfound = paths["/hardware/bytag/{asset_tag}"]["get"]["responses"]["401"]["content"]["text/plain"]
 export type TAsset = paths["/hardware/bytag/{asset_tag}"]["get"]["responses"]["200"]["content"]["application/json"]
+export type TUser = paths["/users/{id}"]["get"]["responses"]["200"]["content"]["application/json"]
